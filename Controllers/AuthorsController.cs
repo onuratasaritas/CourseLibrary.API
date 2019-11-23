ee1ced262d626c6dc4741bb7c9a9a2c1f517b87e
Onur Ata Sarıtaş <onuratasaritas@gmail.com>
2019-11-23 23:43:00 +0300

    Initial commit

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CourseLibrary.API.Services;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+        public IActionResult GetAuthors() {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+
+        }
+    }
+}
