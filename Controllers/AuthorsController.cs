b78174eef3b6d2e4ba61502a0960574b58e150fc
Onur Ata Sarıtaş <onuratasaritas@gmail.com>
2019-11-24 17:43:18 +0300

    Add dto

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CourseLibrary.API.Services;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Helpers;
 
 namespace CourseLibrary.API.Controllers
 {
     [ApiController]
+    [Route("api/authors/")]
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -16,10 +19,50 @@ namespace CourseLibrary.API.Controllers
             _courseLibraryRepository = courseLibraryRepository ??
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
+
+        [HttpGet()]
         public IActionResult GetAuthors() {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-            return new JsonResult(authorsFromRepo);
+            var authors = new List<AuthorDto>();
+            
+            foreach (var author in authorsFromRepo)
+            {
+                authors.Add(new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    MainCategory = author.MainCategory,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                }); 
+                
+            }
+
+            return Ok(authors);
 
         }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return Ok(authorFromRepo);
+            
+        }
+        
+        [HttpPost("{authorId}")]
+        public IActionResult DeleteAuthor(Guid authorId)
+        {
+            if (_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+            //_courseLibraryRepository.DeleteAuthor(authorId);
+            return Ok();
+        }
+        
     }
 }
